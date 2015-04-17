cef042f54f7f5d8c507a3e9a750aac684af60666
zyongbo <zyongbo@users.noreply.github.com>
2015-04-16 21:09:07 -0600

    Update _1_TwoSum.java

--- _1_TwoSum.java
+++ _1_TwoSum.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 /**
  * 
  * @author zyongbo
- * @problem 
+ * @problem Given an array of integers, find two numbers such that they add up to a specific target number
+ * @problem The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2
+ * @problem Please note that your returned answers are not zero-based.
  * 
  */
 
